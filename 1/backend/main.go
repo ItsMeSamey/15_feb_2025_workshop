d0fcce2a6b1bec722455261fb06104bee53eb8d3
ItsMeSamey <sameychain5041@gmail.com>
2025-02-15 17:21:00 +0530

    added backend

---
+++ 1/backend/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+var tasks []string
+
+func getHandler(c fiber.Ctx) (err error) { return c.JSON(tasks) }
+
+func addHandler(c fiber.Ctx) (err error) {
+  task := string(c.Body())
+  tasks = append(tasks, task)
+  return c.SendStatus(http.StatusOK)
+}
+
+func removeHandler(c fiber.Ctx) (err error) {
+  body := c.Body()
+  idx, err := strconv.Atoi(string(body))
+
+  if err != nil { return err }
+
+  if len(tasks) < idx { return errors.New("Invalid index") }
+
+  tasks = append(tasks[:idx-1], tasks[idx:]...)
+  return c.SendStatus(200)
+}
+
+func main() {
+  app := fiber.New()
+  app.Get("/tasks", getHandler)
+  app.Put("/tasks", addHandler)
+  app.Delete("/tasks", removeHandler)
+  app.Listen(":3000")
+}
+
